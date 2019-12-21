eb4c530f339254a5b12496a51042aa8a99cb1650
Ana Guerreiro <anaguerreiro85@hotmail.com>
2019-12-21 23:08:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function ingredientHover(){
+  document.getElementById("ingredient").firstElementChild.firstElementChild.style.fontSize = "300%";
+}
+
+function ingredientNormal(){
+  document.getElementById("ingredient").firstElementChild.firstElementChild.style.fontSize = "100%";
+}
+
+function preparationHover(){
+  document.getElementById("preparation").firstElementChild.firstElementChild.style.fontSize = "300%";
+}
+
+function preparationNormal(){
+  document.getElementById("preparation").firstElementChild.firstElementChild.style.fontSize = "100%";
+}
